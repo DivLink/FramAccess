36249a60bd59098f7d7f031b01aa3e885227d2d9
DivLink Main Frame <mydeveloper2003@gmail.com>
2025-06-17 22:29:00 +0800

    Delete dev-framework directory
    Signed-off-by: DivLink Main Frame <mydeveloper2003@gmail.com>

--- dev-framework/js/load-components.js
+++
@@ -1,6 +0,0 @@
-// fetch("components/navbar.html")
-//   .then(res => res.text())
-//   .then(data => {
-//     document.getElementById("navbar").innerHTML = data;
-//   })
-//   .catch(err => console.error("Navbar load error:", err));
