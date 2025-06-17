86c6b0228051d62399062de65f59fcec17c7ca2b
DivLink Main Frame <mydeveloper2003@gmail.com>
2025-06-17 22:54:46 +0800

    Delete dev-framework directory
    Signed-off-by: DivLink Main Frame <mydeveloper2003@gmail.com>

--- dev-framework/js/frameaccess.js
+++
@@ -1,9 +0,0 @@
-function loadComponent(id, url) {
-  fetch(url)
-    .then(res => res.text())
-    .then(data => {
-      const container = document.getElementById(id);
-      if (container) container.innerHTML = data;
-    })
-    .catch(err => console.error(`Failed to load ${id}:`, err));
-}
