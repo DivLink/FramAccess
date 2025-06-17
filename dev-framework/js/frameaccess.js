fdac852c99b26ae26ec1a1064e408671b2a9db9d
DivLink Main Frame <mydeveloper2003@gmail.com>
2025-06-17 23:11:37 +0800

    Delete dev-framework directory
    Signed-off-by: DivLink Main Frame <mydeveloper2003@gmail.com>

--- dev-framework/js/frameaccess.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  const navLinks = document.getElementById("navLinks");
-  if (navLinks.classList.contains("active")) {
-    navLinks.classList.remove("active");
-  } else {
-    navLinks.classList.add("active");
-  }
-}
